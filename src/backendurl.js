3655da89c01a28d891312e54a6907e0c1409526b
Lokesh Patidar <lokeshbansiya29988@gmail.com>
2024-06-20 11:35:56 +0530

    Again Hide

--- src/backendurl.js
+++ src/backendurl.js
@@ -1,4 +1,3 @@
-const backend_url = `http://localhost:8080`;
-// || process.env.REACT_APP_API_URL;
+const backend_url = process.env.REACT_APP_API_URL;
 console.log({ backend_url });
 export default backend_url;
